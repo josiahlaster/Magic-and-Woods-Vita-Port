28f57f62b9ae2a809665f9580beda66d1b5bc148
Josiah Laster <josiahlaster1@gmail.com>
2025-05-14 13:44:36 -0400

    first commit

---
+++ MagicAndWoodsVitaPort/Player.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+export var speed := 200
+var velocity := Vector2.ZERO
+
+func _process(delta):
+	velocity = Vector2.ZERO
+
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+
+	velocity = velocity.normalized() * speed
+	position += velocity * delta
